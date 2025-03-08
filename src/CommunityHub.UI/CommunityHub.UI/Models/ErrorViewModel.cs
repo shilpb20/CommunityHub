ecc4319916bc4e8fb1d818022ccda4f33498c655
shilpb20 <shilpb20@gmail.com>
2025-03-09 09:01:49 +1100

    Initial project setup.
    Added projects, project references, Github workflow.

---
+++ src/CommunityHub.UI/CommunityHub.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CommunityHub.UI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
